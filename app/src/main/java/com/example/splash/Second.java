b3de9865020afdaa1e51ae00bfc31ba73d06e65a
eshasoni <eshasoni019@gmail.com>
2020-09-18 14:38:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splash/Second.java
@@ -0,0 +1,12 @@
+package com.example.splash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Second extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.second );
+}}
